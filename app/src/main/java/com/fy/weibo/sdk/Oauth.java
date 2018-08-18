1c080696a9f4361cb353b755667c68bfe3d5b236
WwinnerFan <15310610871@163.com>
2018-08-18 11:48:59 +0800

    update

--- app/src/main/java/com/fy/weibo/sdk/Oauth.java
+++ app/src/main/java/com/fy/weibo/sdk/Oauth.java
@@ -9,8 +9,9 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.fy.weibo.R;
-import com.fy.weibo.activity.Main2Activity;
+import com.fy.weibo.activity.MainActivity;
 import com.fy.weibo.util.DataBaseUtil;
+import com.sina.weibo.sdk.auth.AccessTokenKeeper;
 import com.sina.weibo.sdk.auth.Oauth2AccessToken;
 import com.sina.weibo.sdk.auth.WbAuthListener;
 import com.sina.weibo.sdk.auth.WbConnectErrorMessage;
@@ -39,15 +40,16 @@ public class Oauth {
                 String token = oauth2AccessToken.getToken();
                 Log.e(Constants.TAG, oauth2AccessToken.getToken());
                 saveUserData(account, password, token);
+                Constants.ACCESS_TOKEN = oauth2AccessToken.getToken();
+                AccessTokenKeeper.writeAccessToken(activity, oauth2AccessToken);
                 if (activity != null) {
                     activity.runOnUiThread(() -> {
-                        Intent intent = new Intent(activity, Main2Activity.class);
+                        Intent intent = new Intent(activity, MainActivity.class);
                         activity.startActivity(intent);
                         activity.overridePendingTransition(R.anim.in_from_right, R.anim.out_to_left);
                     });
                 }
 
-                Constants.ACCESS_TOKEN = oauth2AccessToken.getToken();
             }
 
             @Override
