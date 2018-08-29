9220053d142d8edddb405302c08e9445ebda5fbc
boomerr <1046702958@qq.com>
2018-08-29 15:42:05 +0800

    罗朕 分享微博

--- app/src/main/java/com/fy/weibo/activity/Main2Activity.java
+++ app/src/main/java/com/fy/weibo/activity/Main2Activity.java
@@ -1,6 +1,7 @@
 package com.fy.weibo.activity;
 
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -22,12 +23,18 @@ import android.widget.Toast;
 import com.fy.weibo.R;
 import com.fy.weibo.fragment.FirstPageFragment;
 import com.fy.weibo.sdk.Constants;
+import com.sina.weibo.sdk.api.ImageObject;
+import com.sina.weibo.sdk.api.TextObject;
+import com.sina.weibo.sdk.api.WeiboMultiMessage;
+import com.sina.weibo.sdk.share.WbShareCallback;
+import com.sina.weibo.sdk.share.WbShareHandler;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements WbShareCallback {
 
     private DrawerLayout drawerLayout;
     private FragmentManager fragmentManager;
     private FloatingActionButton floatButton;
+    private WbShareHandler shareHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +71,8 @@ public class Main2Activity extends AppCompatActivity {
         });
 
         floatButton.setOnClickListener(view -> {
-
+            initWeibo();
+            sendMultiMessage();
         });
     }
 
@@ -95,8 +103,52 @@ public class Main2Activity extends AppCompatActivity {
         saveToken();
         Log.e("TAG", "onDestroy" + "  " + Constants.ACCESS_TOKEN);
     }
+
+
+    /**
+     * 微博分享
+     *
+     *
+     */
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        shareHandler.doResultIntent(intent,this);
+    }
+
+
+    private void initWeibo() {
+        shareHandler = new WbShareHandler(this);
+        shareHandler.registerApp();
+        shareHandler.setProgressColor(0xff33b5e5);
+    }
+    /**
+     * 第三方应用发送请求消息到微博，唤起微博分享界面。
+     */
+    private void sendMultiMessage() {
+        WeiboMultiMessage weiboMessage = new WeiboMultiMessage();
+        weiboMessage.imageObject = new ImageObject();
+        weiboMessage.textObject = new TextObject();
+
+        shareHandler.shareMessage(weiboMessage, false);
+    }
+
+    @Override
+    public void onWbShareSuccess() {
+        Toast.makeText(this,"分享成功", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onWbShareFail() {
+        Toast.makeText(this,"分享失败", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onWbShareCancel() {
+        Toast.makeText(this,"分享取消", Toast.LENGTH_LONG).show();
+    }
 }
 
-/*
-微博展示界面
+/**
+ 微博展示界面
  */
